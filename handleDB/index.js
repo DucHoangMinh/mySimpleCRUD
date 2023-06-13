8d1753b817a9604d188cafa96ea6ad3cc7ad6ca6
Duc Ohlin <hoangminhduc4125@gmail.com>
2023-06-13 14:52:41 +0700

    thay đổi db từ local sang mongodb atlas

--- handleDB/index.js
+++ handleDB/index.js
@@ -1,15 +1,17 @@
 // index.js
+const uri =
+    'mongodb+srv://hoangminhduc4125:hoangminhduc04122003@cluster0.s8cgc8w.mongodb.net/?retryWrites=true&w=majority';
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 4000;
+// const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userInforRoute = require('./accountInfor/userInfor.route');
 const userDataRoute = require('./userData/userDataList.route');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/my_simple_crud', { useNewUrlParser: true }).then(
+mongoose.connect(uri, { useNewUrlParser: true }).then(
     () => {
         console.log('Database is connected');
     },
@@ -27,7 +29,7 @@ app.use('/userdata', userDataRoute);
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳');
 });
-app.listen(PORT, function () {
-    console.log('Server is running on Port:', PORT);
-});
+// app.listen(PORT, function () {
+//     console.log('Server is running on Port:', PORT);
+// });
 module.exports = app;
